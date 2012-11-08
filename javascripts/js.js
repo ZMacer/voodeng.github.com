781d8db8e57e86b632fa58da7db44b4e9de5c150
voodeng <voodeng@gmail.com>
2012-11-09 04:01:49 +0800

    Site updated at 2012-11-08 20:01:49 UTC

--- javascripts/js.js
+++ javascripts/js.js
@@ -1,3 +1,34 @@
+// JavaScript Document
+jQuery.fn.anchorGoWhere = function(options){
+    var obj = jQuery(this);
+    var defaults = {target:1, timer:1000};
+    var o = jQuery.extend(defaults,options);
+    /*
+    var scrollPos;
+    if(typeof document.compatMode != 'undefined' && document.compatMode != 'BackCompat') { 
+        scrollPos = document.documentElement; 
+    }else if (typeof document.body != 'undefined') { 
+        scrollPos = document.body; 
+    }*/
+
+    obj.each(function(i){
+        jQuery(obj[i]).click(function(){
+            var _rel = jQuery(this).attr("href").substr(1);
+            switch(o.target){
+                case 1: 
+                    var targetTop = jQuery("#"+_rel).offset().top;
+                    jQuery("html,body").animate({scrollTop:targetTop}, o.timer);
+                    break;
+                case 2:
+                    var targetLeft = jQuery("#"+_rel).offset().left;
+                    jQuery("html,body").animate({scrollLeft:targetLeft}, o.timer);
+                    break;
+            }
+            return false;
+        });
+    });
+};
+
 jQuery(document).ready(function($) {
     $(".caselist li,#homeweibo .content h2").after("<img src='img/svwloader.gif' class='ldrgif' alt='loading...' />");
 
@@ -23,9 +54,11 @@ jQuery(document).ready(function($) {
         size: 'small'
     });
 
-    $("#footer a").click(function(){
-     htmlbody.animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow','swing');
-     return false;
-     });
+    // $("#wrapper a").click(function(){
+    //  htmlbody.animate({ scrollTop: $($(this).attr("href")).offset().top }, 'slow','swing');
+    //  return false;
+    //  });
+
+$("#wrapper a").anchorGoWhere({target:1});
 
 });
